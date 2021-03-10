eead43a2e1d077ad0def8a29e21b83b9fa17e250
lunex <77702917+lunexdev@users.noreply.github.com>
2021-03-10 17:02:23 +0300

    Add files via upload

---
+++ komutlar/taslak.js
@@ -0,0 +1,17 @@
+const discord = require("discord.js"); //buraya tanımlar
+
+exports.run = async (client, message, args) => {
+
+    //buraya kodlar
+}
+
+exports.conf = {
+    aliases: ["kullanım1" , "kullanım2",], //komutun farklı kullanımı
+    permLevel: 0 //main dosyasında belirtilmiştir
+  }
+
+exports.help = {
+name: "", //komutun ismi
+description: "", //komutun açıklaması
+usage: "" //komutun kullanımı
+}
